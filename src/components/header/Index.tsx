95e7733be5a7cce86079578f206bb9c00a507fdc
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-23 20:48:34 +0530

    updated package file

--- src/components/header/Index.tsx
+++ src/components/header/Index.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
 import styles from './style.module.scss'
+import { HiOutlineMenuAlt4 } from "react-icons/hi";
 
 
 function Header() {
@@ -16,9 +17,9 @@ function Header() {
             <div className=' w-[4.5%] text-[.9vw] text-left'>
                 /Index
             </div>
-            <div className='flex'>
-                <div><span>M</span></div>
-                <div><span>icon</span></div>
+            <div className='w-[5vw] bg-red-400 flex items-center justify-center'>
+                <div className='w-[2vw] h-[2vw] border-b-[1px] border-t-[1px] border-l-[1px] border-r-[1px] rounded-l-full'><span className='text-[.9vw]'>M</span></div>
+                <div className='w-[2vw] h-[2vw] border-b-[1px] border-t-[1px] border-r-[1px] rounded-r-full'><span><HiOutlineMenuAlt4 /></span></div>
             </div>
         </div> 
     </div>
