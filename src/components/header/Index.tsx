cc0fdfb9388479f3273a999dd12d2cf8ade9dcf8
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-23 21:35:48 +0530

    updated index

--- src/components/header/Index.tsx
+++ src/components/header/Index.tsx
@@ -1,22 +1,26 @@
+'use client';
+
 import Link from 'next/link'
 import React from 'react'
 import styles from './style.module.scss'
 import { HiOutlineMenuAlt4 } from "react-icons/hi";
+import { IoCloseOutline } from "react-icons/io5";
+import { motion } from 'framer-motion';
 
 
 function Header() {
     
   return (
-    <div className='w-full sm:px-[2.5vw] sm:py-[2vw] cursor-pointer'>
+    <div className='absolute w-full sm:px-[2.5vw] sm:pt-[2vw] cursor-pointer'>
         <div className='w-full flex items-center justify-between'>
             <div>
                 <Link href="/"> 
                     <h2 className='sm:text-[1.3vw] font-semibold tracking-wider leading-[2vw]'>makepill</h2>
                 </Link>
             </div>
-            <div className=' w-[4.5%] text-[.9vw] text-left'>
-                /Index
-            </div>
+            <motion.div className='uppercase text-[.8vw] text-left'>
+                / Index
+            </motion.div>
             <div className='w-[5vw] flex items-center justify-center'>
                 <div className='w-[2.2vw] h-[2vw] border-b-[1px] border-t-[1px] border-l-[1px] 
                     border-r-[1px] rounded-l-full flex items-center justify-center px-[1.1vw] '
@@ -26,7 +30,12 @@ function Header() {
                 <div className='w-[2.2vw] h-[2vw] border-b-[1px] border-t-[1px] 
                     border-r-[1px] rounded-r-full flex items-center justify-center px-[1.1vw] hover:bg-white hover:text-black'
                 >
-                    <span><HiOutlineMenuAlt4 className='text-[1.2vw]' /></span>
+                    <motion.span>
+                        <HiOutlineMenuAlt4 className='text-[1.2vw]' />
+                    </motion.span>
+                    <motion.span>
+                        <IoCloseOutline className='text-[1.2vw]' />
+                    </motion.span>
                 </div>
             </div>
         </div> 
