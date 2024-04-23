3340e462dea7206af6e62114f2619c4b594588a6
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-23 20:18:02 +0530

    updated index

--- src/components/header/Index.tsx
+++ src/components/header/Index.tsx
@@ -1,9 +1,27 @@
+import Link from 'next/link'
 import React from 'react'
+import styles from './style.module.scss'
+
 
 function Header() {
+    
   return (
-    <div className='w-full bg-green-300'>
-        <div>Header</div> 
+    <div data-scroll-section className='w-full '>
+        <div>
+            <div>
+                <Link href="/"> 
+                    <h2 className='sm:text-[1.5vw] font-semibold leading-[2vw]'>makepill</h2>
+                </Link>
+            </div>
+
+            <div>
+                <h5>/Index</h5>
+            </div>
+
+            <div>
+
+            </div>
+        </div> 
     </div>
   )
 }
