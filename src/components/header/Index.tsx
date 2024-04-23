bbfbcf6238e88a02565ec1efe3570af715ec1ef5
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-23 21:11:18 +0530

    updated index

--- src/components/header/Index.tsx
+++ src/components/header/Index.tsx
@@ -7,7 +7,7 @@ import { HiOutlineMenuAlt4 } from "react-icons/hi";
 function Header() {
     
   return (
-    <div className='w-full sm:px-[2.5vw] sm:py-[2vw]'>
+    <div className='w-full sm:px-[2.5vw] sm:py-[2vw] cursor-pointer'>
         <div className='w-full flex items-center justify-between'>
             <div>
                 <Link href="/"> 
@@ -17,16 +17,16 @@ function Header() {
             <div className=' w-[4.5%] text-[.9vw] text-left'>
                 /Index
             </div>
-            <div className='w-[5vw] bg-red-400 flex items-center justify-center'>
-                <div className='w-[2vw] h-[2vw] border-b-[1px] border-t-[1px] border-l-[1px] 
-                    border-r-[1px] rounded-l-full'
+            <div className='w-[5vw] flex items-center justify-center'>
+                <div className='w-[2.2vw] h-[2vw] border-b-[1px] border-t-[1px] border-l-[1px] 
+                    border-r-[1px] rounded-l-full flex items-center justify-center px-[1.1vw] '
                 >
-                    <span className='text-[.9vw]'>M</span>
+                    <span className='text-[.8vw]'>M</span>
                 </div>
-                <div className='w-[2vw] h-[2vw] border-b-[1px] border-t-[1px] 
-                    border-r-[1px] rounded-r-full'
+                <div className='w-[2.2vw] h-[2vw] border-b-[1px] border-t-[1px] 
+                    border-r-[1px] rounded-r-full flex items-center justify-center px-[1.1vw] hover:bg-white hover:text-black'
                 >
-                    <span><HiOutlineMenuAlt4 /></span>
+                    <span><HiOutlineMenuAlt4 className='text-[1.2vw]' /></span>
                 </div>
             </div>
         </div> 
