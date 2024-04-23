aef1a0d491f2afd2cab8cb9ca94e7b2790acd941
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-23 20:36:20 +0530

    updated index

--- src/components/header/Index.tsx
+++ src/components/header/Index.tsx
@@ -6,20 +6,19 @@ import styles from './style.module.scss'
 function Header() {
     
   return (
-    <div data-scroll-section className='w-full '>
-        <div>
+    <div className='w-full sm:px-[2.5vw] sm:py-[2vw]'>
+        <div className='w-full flex items-center justify-between'>
             <div>
                 <Link href="/"> 
-                    <h2 className='sm:text-[1.5vw] font-semibold leading-[2vw]'>makepill</h2>
+                    <h2 className='sm:text-[1.3vw] font-semibold tracking-wider leading-[2vw]'>makepill</h2>
                 </Link>
             </div>
-
-            <div>
-                <h5>/Index</h5>
+            <div className=' w-[4.5%] text-[.9vw] text-left'>
+                /Index
             </div>
-
-            <div>
-
+            <div className='flex'>
+                <div><span>M</span></div>
+                <div><span>icon</span></div>
             </div>
         </div> 
     </div>
