065e3839e39d6fe8cafacf8b0de2882db8e1c174
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-23 21:35:54 +0530

    updated index

--- src/components/hero/Index.tsx
+++ src/components/hero/Index.tsx
@@ -4,11 +4,12 @@ import Header from '../header/Index'
 function LandingPage() {
   return (
     <div data-scroll-section className='first w-full h-[100vh] overflow-hidden'>
-        <div className='page1'>
-            <div>
+        <div className='page1 relative'>
+            <div className=''>
                 <Header/>
             </div>
-            <div>
+            {/* Hero section video */}
+            <div className='heroVideo'>
                 <video 
                     className='w-full h-[100vh] object-cover'
                     src="https://res.cloudinary.com/mkpill/video/upload/v1691860071/mp_intro_robot_v2_32611febe2.mp4"
@@ -16,7 +17,6 @@ function LandingPage() {
                     loop
                     muted
                 >
-
                 </video>
             </div>
         </div>
