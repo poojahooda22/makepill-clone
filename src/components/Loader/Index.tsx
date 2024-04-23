9b94337ecf76e849ec279d5d83f6f7cda19a7c0d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-23 21:41:31 +0530

    added

--- src/components/Loader/Index.tsx
+++ src/components/Loader/Index.tsx
@@ -14,7 +14,7 @@ function Loader() {
         tl.from('.head4 h4', {
             y: 150,
             opacity: 0,
-        stagger: 0.5,
+            stagger: 0.5,
         duration: .9,
     })
     tl.to('.head4 h4', {
