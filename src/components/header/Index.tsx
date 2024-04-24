0f6ebfa60bc67f3c09ba98e6ead936ac2210844f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-24 08:29:55 +0530

    updated index

--- src/components/header/Index.tsx
+++ src/components/header/Index.tsx
@@ -6,10 +6,11 @@ import styles from './style.module.scss'
 import { HiOutlineMenuAlt4 } from "react-icons/hi";
 import { IoCloseOutline } from "react-icons/io5";
 import { motion } from 'framer-motion';
-
+import { opacity} from './anim' 
+import { useState } from 'react';
 
 function Header() {
-    
+    const [isActive, setIsActive] = useState(false);
   return (
     <div className='header absolute w-full sm:px-[2.5vw] sm:pt-[2vw] cursor-pointer'>
         <div className='w-full flex items-center justify-between'>
@@ -18,7 +19,7 @@ function Header() {
                     <h2 className='sm:text-[1.3vw] font-semibold tracking-wider leading-[2vw]'>makepill</h2>
                 </Link>
             </div>
-            <motion.div className='uppercase text-[.8vw] text-left'>
+            <motion.div variants={opacity} animate={isActive ? 'closed' : 'open'} className='uppercase text-[.8vw] text-left'>
                 / Index
             </motion.div>
             <div className='w-[5vw] flex items-center justify-center'>
@@ -28,12 +29,15 @@ function Header() {
                     <span className='text-[.8vw]'>M</span>
                 </div>
                 <div className='w-[2.2vw] h-[2vw] border-b-[1px] border-t-[1px] 
-                    border-r-[1px] rounded-r-full flex items-center justify-center px-[1.1vw] hover:bg-white hover:text-black'
+                    border-r-[1px] rounded-r-full flex items-center justify-center 
+                    px-[1.1vw] hover:bg-white hover:text-black'
                 >
-                    <motion.span>
+                    <motion.span variants={opacity}
+                        animate={isActive ? 'closed' : 'open'}
+                    >
                         <HiOutlineMenuAlt4 className='text-[1.2vw]' />
                     </motion.span>
-                    <motion.span>
+                    <motion.span  variants={opacity} animate={!isActive ? 'closed' : 'open'}>
                         <IoCloseOutline className='text-[1.2vw]' />
                     </motion.span>
                 </div>
