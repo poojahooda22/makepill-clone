81f8cf476e88ca4b0a459f7b6112923c881dd870
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-23 21:21:52 +0530

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
   watch={
     [
       //.
-      //  Basicaly, you would want to watch 
+      //  Basicaly, you would want to 
       //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
     ]
   }
