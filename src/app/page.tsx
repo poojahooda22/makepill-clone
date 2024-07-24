95586224efbad30e71475db7471f61db780dfa4d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-24 17:40:45 +0530

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
   }
   watch={
     [
-      //  For exemple, on Next.` want to add more criterias if the instance
+      //  For exemple, on Next.` want to add more criterias if the 
     ]
   }
   containerRef={containerRef}
