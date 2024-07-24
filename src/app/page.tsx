40c7525a2779b1f0e3c83a1c9f947aca54892497
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-24 17:41:32 +0530

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
   }
   watch={
     [
-      //  For exemple, on Next.` want to add more 
+      //  For exemple, on Next.` want to add
     ]
   }
   containerRef={containerRef}
