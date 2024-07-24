d561e257f8590ef4ed6ef69b078abacaa8ddd3a1
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-24 16:52:54 +0530

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
   }
   watch={
     [
-      //  For exemple, on Next.` want to add more criterias if the instance  be update on locations
+      //  For exemple, on Next.` want to add more criterias if the instance  be update on 
     ]
   }
   containerRef={containerRef}
