25f5d88e92cd65b0c1de866c8de6011cbdf61e10
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-31 22:25:18 +0530

    added

--- src/components/Cursor/Index.tsx
+++ src/components/Cursor/Index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Index = () => {
+const Cursor = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const Index = () => {
   )
 }
 
-export default Index
+export default Cursor
