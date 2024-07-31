ebf1abfed7e3c5a90785154fb2c564edca8bcf6b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-31 22:09:25 +0530

    updated index

--- src/components/header/Index.tsx
+++ src/components/header/Index.tsx
@@ -21,7 +21,8 @@ function Header() {
                         className='text-[4.5vw] sm:text-[1.3vw] 
                         font-semibold tracking-wider leading-[2vw]'
                     >
-                        makepill</h2>
+                        makepill
+                        </h2>
                 </Link>
             </div>
             <motion.div 
