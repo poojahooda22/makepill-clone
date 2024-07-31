6f515ff2a85914a327fc4cac156231aaccccae7a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-31 22:02:59 +0530

    updated index

--- src/components/header/Index.tsx
+++ src/components/header/Index.tsx
@@ -36,7 +36,7 @@ function Header() {
                 <div
                     onMouseDown={() => {setIsActive(!isActive)}}
                     className={`pill w-[8vw] h-[7vw] sm:w-[2.2vw] sm:h-[2vw] border-b-[1px] 
-                        border-t-[1px] 
+                    border-t-[1px] 
                     border-r-[1px] rounded-r-full flex items-center justify-center 
                     sm:px-[1.1vw] hover:bg-[#f5f5f5] hover:text-[#000]`}
                 >
